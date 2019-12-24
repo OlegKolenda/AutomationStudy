43f85c841256482a5fa46297f74c76950341b073
OlegKolenda <lander28@tut.by>
2019-12-24 20:01:10 +0300

    First commit TestJUnit

--- src/test/java/com/test/TestJUnit.java
+++ src/test/java/com/test/TestJUnit.java
@@ -2,10 +2,20 @@ package com.test;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class TestJUnit {
+
     @Test
-    static void testMethod(){
+    public void testMethod(){
         Assertions.assertEquals(10, 2+8);
+    }
+
+    @ParameterizedTest
+    @ValueSource (ints = {-4,-3,-2,-1,0,1,2,3,4,5})
+    public void testMethod2(int num){
+        Assertions.assertTrue(num > 0 );
+
     }
 }
