a056b2015e5c2eadf2e431ec712ac926e327c441
OlegKolenda <lander28@tut.by>
2020-01-03 21:59:14 +0300

    add junit to project allure

--- src/test/java/com/test/OpenPageTest.java
+++ src/test/java/com/test/OpenPageTest.java
@@ -1,17 +1,52 @@
 package com.test;
 
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 
 public class OpenPageTest {
 
+    private WebDriver driver;
+
     @Test
     @Tag("feature")
-    public void opener(){
+    public void opener() throws MalformedURLException {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://store.apple.com/");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("79.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+
+        driver = new RemoteWebDriver(
+                URI.create("http://ec2-18-217-133-244.us-east-2.compute.amazonaws.com:4444/wd/hub").toURL(),
+                capabilities
+        );
+        //driver = new ChromeDriver();
+        //
+
+        openUrl("http://store.apple.com/");
+        makeScreenshot(driver);
+    }
+
+    @Step("Page opening with url {0}")
+    private void openUrl(String url) {
+        driver.get(url);
+    }
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] makeScreenshot(WebDriver driver) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
 }
